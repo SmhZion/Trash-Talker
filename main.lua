76195f4f4945659815552ea44503d0ff2f696e76
SmhZion <80593689+SmhZion@users.noreply.github.com>
2022-05-17 21:01:46 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,56 +1,56 @@
 local TrashTalkMenu = {
-    "rip bozo",
-    "/ e dab",
-    "/ e laugh",
-    "chewed like a carrot",
-    "need a pillow bro?",
-    "lol",
-    "monkey",
-    "walk straight monkey",
-    "ez dub",
-    "ez lol",
-    "too busy eating watermelon?",
-    "here's a banana",
-    "even the cops aim better",
-    "was the chicken good?",
-    "back in your cage you go",
-    "get good",
-    "where u aiming bro?",
-    "u good bro?",
-    "get better",
-    "isnt that hard to aim",
-    "bad player right here",
-    "just play the game right",
-    "wasnt so hard",
-    "should get good",
-    "bro im right here?"
+	"rip bozo",
+	"/ e dab",
+	"/ e laugh",
+	"chewed like a carrot",
+	"need a pillow bro?",
+	"lol",
+	"monkey",
+	"walk straight monkey",
+	"ez dub",
+	"ez lol",
+	"too busy eating watermelon?",
+	"here's a banana",
+	"even the cops aim better",
+	"was the chicken good?",
+	"back in your cage you go",
+	"get good",
+	"where u aiming bro?",
+	"u good bro?",
+	"get better",
+	"isnt that hard to aim",
+	"bad player right here",
+	"just play the game right",
+	"wasnt so hard",
+	"should get good",
+	"bro im right here?"
 }
 local UserInputService = game:GetService("UserInputService")
 UserInputService.InputBegan:Connect(function(Key)
-    if Key.KeyCode == Enum.KeyCode.T then
-        local ChosenTrashText = math.random(1, #TrashTalkMenu)
-        local args = {
-            [1] = TrashTalkMenu[ChosenTrashText],
-            [2] = "All"
-        }
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-    end
+	if Key.KeyCode == Enum.KeyCode.Minus then
+		local ChosenTrashText = math.random(1, #TrashTalkMenu)
+		local args = {
+			[1] = TrashTalkMenu[ChosenTrashText],
+			[2] = "All"
+		}
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+	end
 end)
 wait(0.2)
 game.StarterGui:SetCore("SendNotification",{
-    Title = "Trash Talker";
-    Text = "Use this at your own risk, we are not responsible for **ANY** Bans from Roblox or in Game.";
-    Duration = 5;
+	Title = "Trash Talker";
+	Text = "Use this at your own risk, we are not responsible for **ANY** Bans from Roblox or in Game.";
+	Duration = 10;
 })
 wait(0.2)
 game.StarterGui:SetCore("SendNotification",{
-    Title = "Trash Talker";
-    Text = "Press ''T'' to Trash Talk.";
-    Duration = 5;
+	Title = "Trash Talker";
+	Text = "Press ''-'' to Trash Talk.";
+	Duration = 10;
 })
 wait(0.2)
 game.StarterGui:SetCore("SendNotification",{
-    Title = "Trash Talker";
-    Text = "Credits: SmhZion#6012";
-    Duration = 5;
+	Title = "Trash Talker";
+	Text = "Credits: SmhZion#6012";
+	Duration = 5;
 })
